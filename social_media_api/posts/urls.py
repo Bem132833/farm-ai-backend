00f00ef05691cb66da235000be3c01f41c88b3be
Bemigbar Yehuwalawork <bemigbaryehuwalawork@gmail.com>
2026-06-23 21:57:23 +0300

    Delete social_media_api directory

--- social_media_api/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import LikePostAPIView, UnlikePostAPIView
-
-urlpatterns = [
-    path('<int:pk>/like/', LikePostAPIView.as_view(), name='like-post'),
-    path('<int:pk>/unlike/', UnlikePostAPIView.as_view(), name='unlike-post'),
-]
